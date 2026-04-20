29d501ea145de394c22d89b3792e51ad42790c9a
Christer <christer.tjader@munckstudios.com>
2026-04-20 06:27:02 +0200

    Initial Commit

---
+++ ingest_SMTMUNCK/ingest_SMTMUNCKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ingest_SMTMUNCKApp.swift
+//  ingest_SMTMUNCK
+//
+//  Created by sysmunck on 2026-04-20.
+//
+
+import SwiftUI
+
+@main
+struct ingest_SMTMUNCKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
